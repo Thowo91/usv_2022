32870520df851a5aef86e162d8d7d19ea56b9234
Thomas Worofsky <tworofsky@mp-group.net>
2022-09-07 11:02:47 +0200

    Fonts Noto

--- functions.php
+++ functions.php
@@ -7,12 +7,14 @@ define('JS', ASSETS . '/js');
 
 function add_usv_scripts()
 {
-    wp_enqueue_style('usv-style', THEMEDIR . '/style.css', [], null);
-
     wp_enqueue_style('bootstrap', CSS . '/bootstrap.min.css', [], null);
     wp_enqueue_script('bootstrap', JS . '/bootstrap.bundle.min.js');
-
-    wp_enqueue_style( 'fontawesome', ASSETS. '/fontawesome/css/all.css', [], null );
+    
+    wp_enqueue_style('fontawesome', ASSETS. '/fontawesome/css/all.css', [], null );
+    
+    wp_enqueue_style('fonts', ASSETS. '/fonts/fonts.css', [], null );
+    
+    wp_enqueue_style('usv-style', THEMEDIR . '/style.css', [], null);
 }
 
 add_action('wp_enqueue_scripts', 'add_usv_scripts');
